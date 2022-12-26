1ad1e4125c82e24857eb16a628d31f99a574ef64
Marfolo <matti0@onet.pl>
2022-12-26 10:25:15 +0100

    More code

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 from time_calculator import add_time
 
+print(add_time("12:20 AM", "5:50"))
+
+input("Enter to end")
--- time_calculator.py
+++ time_calculator.py
@@ -6,5 +6,18 @@ def add_time(start_time, duration_time):
 	dur1, dur2 = duration_time.split(':')
 	sta1, sta2 = temp1.split(':')
 
+	dur1, dur2 = int(dur1), int(dur2)
+	sta1, sta2 = int(sta1), int(sta2)
 
-	return new_time
+	#PAY CLOSE ATTENTION TO THE SPACING AND PUNCTUATION OF THE RESULTS
+	if sta1 > 12 or sta1 < 1:
+		print("Error")
+	elif sta2 >=60 or sta2 < 0:
+		print("Error")
+	elif dur2 >=60 or dur2 < 0:
+		print("Error")
+	else:
+		print("hi")
+
+
+	return 1#new_time
