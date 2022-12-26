a858d8056eb9317e874b84fb36a3d2db6886d416
Marfolo <matti0@onet.pl>
2022-12-26 10:09:24 +0100

    Added 3 lines of code

--- time_calculator.py
+++ time_calculator.py
@@ -1,3 +1,10 @@
 def add_time(start_time, duration_time):
-	
+	#start_time -> ["12:20 AM"]
+	#duration_time -> ["4:40"]
+
+	temp1, time_symbol = start_time.split()
+	dur1, dur2 = duration_time.split(':')
+	sta1, sta2 = temp1.split(':')
+
+
 	return new_time
